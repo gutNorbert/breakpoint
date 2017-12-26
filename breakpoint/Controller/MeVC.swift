b6896fa17378ca646755fe1c7c591a61d389479d
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-26 19:09:07 +0100

    MeVC

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Gutpinter Norbert on 2017. 12. 25..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+
+}
