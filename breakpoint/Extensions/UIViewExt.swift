a02bc80b82f3cb404ca92ed12c9b183699112b83
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-27 13:40:59 +0100

    binding views

---
+++ breakpoint/Extensions/UIViewExt.swift
@@ -0,0 +1,27 @@
+//
+//  UIViewExt.swift
+//  breakpoint
+//
+//  Created by Gutpinter Norbert on 2017. 12. 26..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import UIKit
+
+extension UIView{
+    func bindToKeyboard(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification){
+        
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double //Dictionary
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let beginningFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let deltaY = endFrame.origin.y - beginningFrame.origin.y
+        
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0.0, options: UIViewKeyframeAnimationOptions(rawValue: curve), animations: {self.frame.origin.y += deltaY}, completion: nil)
+    }
+}
