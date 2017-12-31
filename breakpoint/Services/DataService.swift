189de3dc0c141006f222a548c8e6fe0bdb711395
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-31 19:03:02 +0100

    Downloading messages

--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -53,7 +53,8 @@ class DataService {
     
     func uploadPost(withMessage message: String, forUID uid: String, withGroupKey groupkey: String?, sendComplete: @escaping(_ status: Bool) -> ()){
         if groupkey != nil {
-            //send to groups ref
+            REF_GROUPS.child(groupkey!).child("messages").childByAutoId().updateChildValues(["content": message,"senderId": uid])
+            sendComplete(true)
         } else {
             REF_FEED.childByAutoId().updateChildValues(["content": message, "senderId" : uid])
             sendComplete(true)
@@ -72,6 +73,21 @@ class DataService {
                 messageArray.append(message)
             }
             handler(messageArray)
+        }
+    }
+    
+    func getAllMessagesFor(desiredGroup: Group, handler: @escaping (_ messagesArray: [Message]) -> ()){
+        var groupMessageArray = [Message]()
+        REF_GROUPS.child(desiredGroup.key).child("messages").observeSingleEvent(of: .value) { (groupMessageSnapshot) in
+            guard let groupMessageSnapshot = groupMessageSnapshot.children.allObjects as? [DataSnapshot] else {return}
+            for groupMessage in groupMessageSnapshot {
+                let content = groupMessage.childSnapshot(forPath: "content").value as! String
+                let senderId = groupMessage.childSnapshot(forPath: "senderId").value as! String
+                let groupMessage = Message(content: content, senderId: senderId)
+                groupMessageArray.append(groupMessage)
+                
+            }
+            handler(groupMessageArray)
         }
     }
     
