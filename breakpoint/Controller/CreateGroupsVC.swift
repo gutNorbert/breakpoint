6e6fab3ed789cf7d0fc0a0c851cb33a8c23ef0e8
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-31 14:27:31 +0100

    pushing groups to firebase

--- breakpoint/Controller/CreateGroupsVC.swift
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateGroupsVC: UIViewController {
 
@@ -47,6 +48,20 @@ class CreateGroupsVC: UIViewController {
     }
     
     @IBAction func doneBtnWasPressed(_ sender: Any) {
+        if titleTextField.text != "" && descriptionTextField.text != "" {
+            DataService.instance.getIds(forUsernames: chosenUserArray, handler: { (idsArray) in
+                var userIds = idsArray
+                userIds.append((Auth.auth().currentUser?.uid)!)
+                
+                DataService.instance.createGroup(withTitle: self.titleTextField.text!, andDescription: self.descriptionTextField.text!, forUserIds: userIds, handler: { (groupCreated) in
+                    if groupCreated{
+                        self.dismiss(animated: true, completion: nil)
+                    } else {
+                        print("Group could not be created, Please try again.")
+                    }
+                })
+            })
+        }
     }
     
     @IBAction func closeBtnWasPressed(_ sender: Any) {
@@ -71,6 +86,7 @@ extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource{
         } else {
              cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: false)
         }
+        
         return cell
     }
     
