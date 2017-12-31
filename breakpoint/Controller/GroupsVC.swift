1215f86f0a0f83a1b0a77bedfd832f7f0fc6f928
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-12-31 19:18:49 +0100

    UIViewController Ext

--- breakpoint/Controller/GroupsVC.swift
+++ breakpoint/Controller/GroupsVC.swift
@@ -53,6 +53,6 @@ extension GroupsVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let groupFeedVC = storyboard?.instantiateViewController(withIdentifier: "GroupFeedVC") as? GroupFeedVC else { return }
         groupFeedVC.initGroupData(forGroup: groupsArray[indexPath.row])
-        present(groupFeedVC, animated: true, completion: nil)
+        presentDetail(groupFeedVC)
     }
 }
